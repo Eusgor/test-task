36e01fe8776f8ad40fdce2eb7784441d337dc0a9
Evgeniy Gorbanev <gorbanev.es@gmail.com>
2023-06-04 19:52:28 +0600

    Add argparse and change package comparison method

--- compare.py
+++ compare.py
@@ -3,27 +3,47 @@ import json
 
 
 def get_packages(branch):
-    url = f"https://rdb.altlinux.org/api/export/branch_binary_packages/{branch}?arch=x86_64"
-    response = requests.get(url)
-    response.raise_for_status()
+    url = f"https://rdb.altlinux.org/api/export/branch_binary_packages/{branch}"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except Exception as e:
+        print(f'Error: {str(e)}')
+        print("Use branches sisyphus, p10 or p9")
+        exit(1)
     return json.loads(response.text)
+
+def create_dict(data):
+    dct = {}
+    for pkg in data:
+        dct.setdefault(pkg["arch"], {})
+        dct[pkg["arch"]][pkg["name"]] = pkg
+    return dct
 
 def compare(branch1, branch2):
     data1 = get_packages(branch1)["packages"]
     data2 = get_packages(branch2)["packages"]
 
-    data1_names = set(pkg["name"] for pkg in data1)
-    data2_names = set(pkg["name"] for pkg in data2)
-    data_names = data1_names.intersection(data2_names)
+    pkg_dict1 = create_dict(data1)
+    pkg_dict2 = create_dict(data2)
 
-    only_in1 = [pkg for pkg in data1 if pkg["name"] not in data2_names]
-    only_in2 = [pkg for pkg in data2 if pkg["name"] not in data1_names]
+    result = {}
+    for arch, val in pkg_dict1.items():
+        data1_names = set(pkg_dict1[arch].keys())
+        data2_names = set(pkg_dict2[arch].keys())
+        data_names = data1_names.intersection(data2_names)
+        
+        only_in1 = [val[pkg] for pkg in val 
+                    if pkg not in data2_names]
+        only_in2 = [pkg_dict2[arch][pkg] for pkg in pkg_dict2[arch] 
+                    if pkg not in data1_names]
+        
+        result.setdefault(arch, {})
+        result[arch][f"only_in_{branch1}"] = only_in1
+        result[arch][f"only_in_{branch2}"] = only_in2
 
+    wtfile(result, "result")
 
-    wtfile(only_in1, "only_in1")
-    wtfile(only_in2, "only_in2")
-
-#for testing
 def wtfile(data, file):
     with open(file, 'w', encoding='utf-8') as file:
         json.dump(data, file, indent=4)
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from compare import compare 
+from compare import compare
+import argparse
 
 
 def main():
-    compare("p10", "p9")
+    parser = argparse.ArgumentParser(description='Compare binary packages between two branches')
+    parser.add_argument('branch1', help='First branch to compare')
+    parser.add_argument('branch2', help='Second branch to compare')
+    args = parser.parse_args()
+
+    compare(args.branch1, args.branch2)
+
 
 if __name__ == '__main__':
     main()
