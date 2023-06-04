ade29fcaaac4394344051609e4db285647757b54
Evgeniy Gorbanev <gorbanev.es@gmail.com>
2023-06-04 15:32:27 +0600

    Get a list of packages
    Get a list of packages that are only in the first or second branch

--- compare.py
+++ compare.py
@@ -12,11 +12,20 @@ def compare(branch1, branch2):
     data1 = get_packages(branch1)["packages"]
     data2 = get_packages(branch2)["packages"]
 
-    wtfile(data1)
+    data1_names = set(pkg["name"] for pkg in data1)
+    data2_names = set(pkg["name"] for pkg in data2)
+    data_names = data1_names.intersection(data2_names)
+
+    only_in1 = [pkg for pkg in data1 if pkg["name"] not in data2_names]
+    only_in2 = [pkg for pkg in data2 if pkg["name"] not in data1_names]
+
+
+    wtfile(only_in1, "only_in1")
+    wtfile(only_in2, "only_in2")
 
 #for testing
-def wtfile(data):
-    with open('data.json', 'w', encoding='utf-8') as file:
+def wtfile(data, file):
+    with open(file, 'w', encoding='utf-8') as file:
         json.dump(data, file, indent=4)
 
     
